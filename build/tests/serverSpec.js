fc5f8a58aea3ff75482d77dbbc7d14f1908a1f30
Ahmed Youssef <ahmed7777youssef@gmail.com>
2022-12-06 16:22:06 +0200

    project setup (typescript, prettier, jasmine, eslint)

---
+++ build/tests/serverSpec.js
@@ -0,0 +1,7 @@
+"use strict";
+let x = 25;
+describe('the first test in the project :)', () => {
+    it('varible x must be equal to 25', () => {
+        expect(x).toEqual(25);
+    });
+});
---
+++ src/tests/serverSpec.ts
@@ -0,0 +1,7 @@
+let x: number = 25;
+
+describe('the first test in the project :)', () => {
+    it('varible x must be equal to 25', () => {
+        expect(x).toEqual(25);
+    });
+});
